ebb5c1b6c7215d147db564d6d0fa7cdf277cca4a
ObsidianVeil <Hufwe.makto@gmail.com>
2025-03-03 09:54:09 +0000

    Changing priorities of inputs. Will now prioritise input from INSERT_YOUR_FILES_HERE directory over the defs file.

--- Tbrucei_project/master_script.sh
+++ Tbrucei_project/master_script.sh
@@ -18,29 +18,33 @@ conda activate python
 echoerr() { cat <<< "$@" 1>&2; }
 outputdir=./output/
 
+#export variables
+export echoerr
+
 #extracting user input filename and output directory
 defs_fileloc=$(grep "^File Location:" ./defs | cut -d ":" -f 2-)
 defs_directory=$(grep "^Output Location:" ./defs | cut -d ":" -f 2-)
 
 #if file location in defs file does not exist, check if INSERT_YOUR_FILES_HERE is empty. 
 #If no, set file location to INSERT_YOUR_FILES_HERE. If yes, end
-if ! [ -f ./$defs_fileloc]; then
-	echo "File location does not exist"
-	if find ./INSERT_YOUR_FILES_HERE -mindepth 1 -maxdepth 1 | read; then
-		filelocation=./INSERT_YOUR_FILES_HERE
-		echo "INSERT_YOUR_FILES_HERE input folder selected"
+
+if find ./INSERT_YOUR_FILES_HERE -mindepth 1 -maxdepth 1 | read; then
+	filelocation=./INSERT_YOUR_FILES_HERE
+	echo "INSERT_YOUR_FILES_HERE folder selected as input"
 	else
-		echoerr("Error: No directory entered and INSERT_YOUR_FILES_HERE directory empty. Ending the process")
-		exit 1
-	fi
-else
-	echo "File location does exist"
-	filelocation=./defs_fileloc
+		if ! [ -f ./$defs_fileloc]; then
+			filelocation=./$defs_fileloc
+			echo "Input file location obtained from defs"
+		else 
+			echoerr("Error: No directory entered in defs doc and INSERT_YOUR_FILES_HERE directory empty. Ending the process")
+			exit 1
+		fi
 fi
+
 	
 #test if output directory exists
 if ! [ -d ./$defs_directory]; then
-	echo "Warning: Output Directory does not exist. Outputting to default"
+	echo "Warning: Output directory from defs document does not exist. Outputting to default."
 	mkdir -p ./outputs
 else
 	outputdir=$defs_directory
