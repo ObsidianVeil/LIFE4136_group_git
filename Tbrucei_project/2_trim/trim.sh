c1acd53da5fd585979fd05c91b02e5aec6a2fc4a
ObsidianVeil <Hufwe.makto@gmail.com>
2025-03-07 10:22:27 +0000

    Trying to get trim to work correctly

--- Tbrucei_project/2_trim/trim.sh
+++ Tbrucei_project/2_trim/trim.sh
@@ -16,24 +16,26 @@ source $HOME/.bash_profile
 echo "Running trim"
 
 # List of files to process 
-FILES=(
-    "Human1.fastq.gz"
-    "Human2.fastq.gz"
-    "Human3.fastq.gz"
-    "Human4.fastq.gz"
-    "Human5.fastq.gz"
-    "Rat1.fastq.gz"
-    "Rat2.fastq.gz"
-    "Rat3.fastq.gz"
-    "Rat4.fastq.gz"  
-    "Rat5.fastq.gz"
-)
+#FILES=(
+#    "Human1.fastq.gz"
+#    "Human2.fastq.gz"
+#    "Human3.fastq.gz"
+#    "Human4.fastq.gz"
+#    "Human5.fastq.gz"
+#    "Rat1.fastq.gz"
+#    "Rat2.fastq.gz"
+#    "Rat3.fastq.gz"
+#    "Rat4.fastq.gz"  
+#    "Rat5.fastq.gz"
+#)
+
+FILES=ls *.fastq.gz
 
 mkdir -p $outputdir/trim
 
 # Run Trim Galore on all samples i have used for so we can keep the code clean  
-for FILE in *.fastq.gz; do
-    trim_galore --fastqc --quality 28  --output_dir $outputdir/trim "$filelocation/$FILE"
+for FILE in "$filelocation"/*.fastq.gz; do
+    trim_galore --fastqc --quality 28  --output_dir $outputdir/trim "$FILE"
 done
 
 #for FILE in "${FILES[@]}"; do
