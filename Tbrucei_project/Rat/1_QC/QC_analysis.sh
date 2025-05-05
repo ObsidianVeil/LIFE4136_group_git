f29fd4f0e01e16d050baff4d5203cf4e38e7a823
ObsidianVeil <Hufwe.makto@gmail.com>
2025-05-05 17:33:58 +0100

    Updated 1_QC code to reflect change in structure.

--- Tbrucei_project/Rat/1_QC/QC_analysis.sh
+++ Tbrucei_project/Rat/1_QC/QC_analysis.sh
@@ -6,16 +6,21 @@
 #SBATCH --mem=100G
 #SBATCH --time=02:00:00
 #SBATCH --job-name=fastqc_analysis 
-#SBATCH --output=./logs/FastQC/%j-%x.out
-#SBATCH --error=./logs/FastQC/%j-%n.err
+#SBATCH --output=./FASTQC/logs/%j-%x.out
+#SBATCH --error=./FASTQC/logs/%j-%n.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=mbxsh12@nottingham.ac.uk
 
 source $HOME/.bash_profile
 
+conda activate Python #Change Python to the name of the relevant conda environment
+
+filelocation="/share/BioinfMSc/rotation2/Group1/LIFE4136_group_git/Tbrucei_project/Rat/INSERT_FILES_HERE"
+outputdir="/share/BioinfMSc/rotation2/Group1/LIFE4136_group_git/Tbrucei_project/Rat/1_QC"
+
 echo "Running QC analysis"
 
-#Make 
+#Make output directory
 mkdir -p $outputdir/FastQC
 
 # Run FastQC on all FASTQ files
